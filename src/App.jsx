b15c935f485e7db439b6eab2ff5b0cf403be3dae
serkanursavas <serkan.ursavas@icloud.com>
2023-04-01 22:44:31 +0300

    adding some styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import './App.css'
 import Signup from './pages/Signup'
+import { ConfigProvider } from 'antd'
 
 function App() {
   return (
-    <>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: '#6096B4'
+        }
+      }}
+    >
       <Signup />
-    </>
+    </ConfigProvider>
   )
 }
 
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,12 +1,69 @@
-import { Form, Input, Button, Upload } from 'antd'
-import { UserOutlined, LockOutlined, MailOutlined, PlusOutlined } from '@ant-design/icons'
+import { Form, Input, Button, Upload, message } from 'antd'
+import { UserOutlined, LockOutlined, MailOutlined, PlusOutlined, LoadingOutlined } from '@ant-design/icons'
+
+import { useState } from 'react'
+
+const getBase64 = (img, callback) => {
+  const reader = new FileReader()
+  reader.addEventListener('load', () => callback(reader.result))
+  reader.readAsDataURL(img)
+}
+const beforeUpload = file => {
+  const isJpgOrPng = file.type === 'image/jpeg' || file.type === 'image/png'
+  if (!isJpgOrPng) {
+    message.error('You can only upload JPG/PNG file!')
+  }
+  const isLt2M = file.size / 1024 / 1024 < 2
+  if (!isLt2M) {
+    message.error('Image must smaller than 2MB!')
+  }
+  return isJpgOrPng && isLt2M
+}
 
 const Signup = () => {
+  const [form] = Form.useForm()
+
+  const onFinish = values => {
+    console.log(values)
+  }
+
+  const [loading, setLoading] = useState(false)
+  const [imageUrl, setImageUrl] = useState()
+  const handleChange = info => {
+    if (info.file.status === 'uploading') {
+      setLoading(true)
+      return
+    }
+    if (info.file.status === 'done') {
+      // Get this url from response in real world.
+      getBase64(info.file.originFileObj, url => {
+        setLoading(false)
+        setImageUrl(url)
+      })
+    }
+  }
+  const uploadButton = (
+    <div>
+      {loading ? <LoadingOutlined /> : <PlusOutlined />}
+      <div
+        style={{
+          marginTop: 8
+        }}
+      >
+        Upload
+      </div>
+    </div>
+  )
+
   return (
     <div className="p-5 text-center bg-white border border-gray-200 border-solid rounded-md shadow-md w-72">
       <h3 className="mb-2 text-3xl text-primary">YOUR CHAT</h3>
       <h4 className="mt-2 font-thin">Register</h4>
-      <Form>
+      <Form
+        form={form}
+        onFinish={onFinish}
+        requiredMark={true}
+      >
         <Form.Item
           name="name"
           rules={[
@@ -18,7 +75,8 @@ const Signup = () => {
         >
           <Input
             prefix={<UserOutlined />}
-            placeholder="Name"
+            placeholder=" Name"
+            className="custom-input !shadow-none"
           />
         </Form.Item>
         <Form.Item
@@ -36,10 +94,12 @@ const Signup = () => {
         >
           <Input
             prefix={<MailOutlined />}
-            placeholder="Email"
+            placeholder=" Email"
+            className="custom-input !shadow-none"
           />
         </Form.Item>
         <Form.Item
+          required
           name="password"
           rules={[
             {
@@ -54,29 +114,47 @@ const Signup = () => {
         >
           <Input.Password
             prefix={<LockOutlined />}
-            placeholder="Password"
+            placeholder=" Password"
+            className="custom-input !shadow-none"
           />
         </Form.Item>
         <Upload
           name="avatar"
           listType="picture-circle"
+          className="avatar-uploader"
+          showUploadList={false}
+          beforeUpload={beforeUpload}
+          action="https://www.mocky.io/v2/5cc8019d300000980a055e76"
+          onChange={handleChange}
         >
-          <div>
-            <PlusOutlined />
-            <div className="mt-0">Add Photo</div>
-          </div>
+          {imageUrl ? (
+            <img
+              src={imageUrl}
+              alt="avatar"
+              style={{
+                width: '100%'
+              }}
+            />
+          ) : (
+            uploadButton
+          )}
         </Upload>
-        <Form.Item>
-          <Button
-            type="primary"
-            htmlType="submit"
-            className="w-full mt-4 font-medium"
-          >
-            SIGN UP
-          </Button>
+        <Form.Item shouldUpdate>
+          {() => (
+            <Button
+              type="primary"
+              htmlType="submit"
+              className="w-full mt-4 font-medium"
+              disabled={
+                !form.isFieldsTouched(true) || !!form.getFieldsError().filter(({ errors }) => errors.length).length
+              }
+            >
+              Sign Up
+            </Button>
+          )}
         </Form.Item>
       </Form>
-      <p className="font-light">
+      <p className="text-sm font-light">
         You already have an account?{' '}
         <a
           href="#"
