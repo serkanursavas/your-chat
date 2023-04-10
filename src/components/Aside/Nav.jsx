9ce422176cc9993c2ae12f06b29d5cb1b6561129
serkanursavas <serkan.ursavas@icloud.com>
2023-04-10 04:04:48 +0300

    add login auth and error handling

--- src/components/Aside/Nav.jsx
+++ src/components/Aside/Nav.jsx
@@ -4,6 +4,8 @@ import { signOut } from 'firebase/auth'
 import { auth } from '../../store/firebase'
 
 import ProfilePhoto from '../ProfilePhoto'
+import { useContext } from 'react'
+import { AuthContext } from '../../context/AuthContext'
 
 const Nav = () => {
   const items = [
@@ -21,6 +23,8 @@ const Nav = () => {
     }
   ]
 
+  const { currentUser } = useContext(AuthContext)
+
   return (
     <div className="flex items-center justify-between px-2 h-14 bg-primary">
       <div className="font-semibold text-lg text-[#C9DFE7]">Your Chat</div>
@@ -35,7 +39,7 @@ const Nav = () => {
           <div className="h-10 p-1 rounded-full cursor-pointer hover:bg-secondary">
             <ProfilePhoto
               size="40"
-              profilePhoto="https://images.pexels.com/photos/5384445/pexels-photo-5384445.jpeg?auto=compress&cs=tinysrgb&w=1200"
+              profilePhoto={currentUser.photoURL}
             />
           </div>
         </Dropdown>
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,4 +1,4 @@
-import { Form, Input, Button, Upload, Alert, notification } from 'antd'
+import { Form, Input, Button, Upload, notification } from 'antd'
 import { UserOutlined, LockOutlined, MailOutlined, PlusOutlined, LoadingOutlined } from '@ant-design/icons'
 import { useState, useRef } from 'react'
 import { createUserWithEmailAndPassword, updateProfile } from 'firebase/auth'
