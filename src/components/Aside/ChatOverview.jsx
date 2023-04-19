91791254c7847e0b192d3f4767b1614bc190a102
serkanursavas <serkan.ursavas@icloud.com>
2023-04-19 19:03:56 +0300

    fixing overflowing msg

--- src/components/Aside/ChatOverview.jsx
+++ src/components/Aside/ChatOverview.jsx
@@ -1,19 +1,21 @@
 import ProfilePhoto from '../ProfilePhoto'
 
-const ChatOverview = props => {
+const ChatOverview = ({ lastMessage, profilePhoto, username, active }) => {
+  const modifiedLastMessage = lastMessage.length > 24 ? `${lastMessage.slice(0, 24)}...` : lastMessage
+
   return (
     <div
       className={`flex items-center py-2 space-x-2 h-fit ${
-        props.active ? 'bg-[#7AABC2]' : ''
+        active ? 'bg-[#7AABC2]' : ''
       } hover:bg-primary  active:bg-primary hover:cursor-pointer px-3 `}
     >
       <ProfilePhoto
         size="50"
-        profilePhoto={props.profilePhoto}
+        profilePhoto={profilePhoto}
       />
       <div className="flex flex-col justify-center">
-        <span className="font-medium ">{props.username}</span>
-        <span className="text-sm opacity-40">{props.lastMessage}</span>
+        <span className="font-medium ">{username}</span>
+        <span className="text-sm break-all opacity-40">{modifiedLastMessage}</span>
       </div>
     </div>
   )
